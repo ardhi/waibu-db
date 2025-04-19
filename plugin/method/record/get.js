8277e52ef05f7aad5a2d09f2a4e799faf66ee992
Ardhi Lukianto <ardhi@lukianto.com>
2025-04-19 11:26:10 +0700

    Updates

--- plugin/method/record/get.js
+++ plugin/method/record/get.js
@@ -1,12 +1,10 @@
 import prepCrud from '../../../lib/prep-crud.js'
 
 async function get ({ model, req, reply, id, options = {} }) {
-  const { recordFindOne, attachmentFind } = this.app.dobo
-  const { name, filter, opts, attachment, stats, mimeType } = prepCrud.call(this, { model, req, reply, id, options, args: ['model', 'id'] })
+  const { recordFindOne } = this.app.dobo
+  const { name, filter, opts } = prepCrud.call(this, { model, req, reply, id, options, args: ['model', 'id'] })
   filter.query = { $and: [filter.query ?? {}, { id: id ?? req.params.id }] }
-  opts.dataOnly = true
   const ret = await recordFindOne(name, filter, opts)
-  if (attachment) ret.data._attachment = await attachmentFind(name, ret.id, { stats, mimeType })
   return ret
 }
 
